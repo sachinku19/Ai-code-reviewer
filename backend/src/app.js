fbc213541b2371dd3bdfe5689d34920a17f6904c
sachinku19 <mrs201171@gmail.com>
2026-05-22 12:06:12 +0530

    added home route

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,6 +21,9 @@ app.use(
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+app.get("/", (req, res) => {
+    res.send("Backend Running Successfully");
+});
 app.use(
   "/api",
   rateLimit({
